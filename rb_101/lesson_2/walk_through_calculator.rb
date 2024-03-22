7d500358d55d546308ff66bbe0a66d6d9d6a6377
Jordan Adamson <jordanadamson540@gmail.com>
2024-03-22 09:59:23 -0500

    all extra features added but slightly incomplete due to yaml interpolation

--- rb_101/lesson_2/walk_through_calculator.rb
+++ rb_101/lesson_2/walk_through_calculator.rb
@@ -1,8 +1,16 @@
 require 'yaml'
 MESSAGES = YAML.load_file('calculator_messages.yml')
 
-def prompt(message)
+LANGUAGE = 'en'
+
+def prompt(key)
+  message = messages(key, LANGUAGE)
   Kernel.puts("=> #{message}")
+end
+
+def interpolation_prompt(key, interpolation)
+  message = messages(key, LANGUAGE)
+  Kernel.puts("=> #{message} #{interpolation}")
 end
 
 def valid_number?(num)
@@ -19,70 +27,79 @@ end
 
 def operation_to_message(operation)
   word = case operation
-           when '1'
-             'Adding'
-           when '2'
-             'Subtracting'
-           when '3'
-             'Multiplying'
-           when '4'
-             'Dividing'
+         when '1'
+           '=> Adding'
+         when '2'
+           '=> Subtracting'
+         when '3'
+           '=> Multiplying'
+         when '4'
+           '=> Dividing'
          end
 
   x = "A random line of code"
 
-  word
+  puts word + " the two numbers..."
 end
 
-prompt(MESSAGES['welcome'])
+def messages(message, lang='en')
+  MESSAGES[lang][message]
+end
+
+
+
+prompt('welcome')
 
 name = ''
 loop do
   name = Kernel.gets().chomp()
 
   if name.empty?
-    prompt(MESSAGES['valid_name'])
+    prompt('valid_name')
   else
     break
   end
 end
 
-prompt(MESSAGES['hello'] + " #{name}! ")
+#prompt('hello')#,"#{name}!") +  
+
+#puts "Hi #{name}!" #handle interpolation by using the prompt and not using yaml
+interpolation_prompt('hello', name)
 
 loop do # main loop
   number1 = ''
   loop do
-    prompt(MESSAGES['number1'])
+    prompt('number1')
     number1 = Kernel.gets().chomp()
 
     if valid_number?(number1)
       break
     else
-      prompt(MESSAGES['incorrect_number'])
+      prompt('incorrect_number')
     end
   end
 
   number2 = ''
   loop do
-    prompt(MESSAGES['number2'])
+    prompt('number2')
     number2 = Kernel.gets().chomp()
 
     if valid_number?(number2)
       break
     else
-      prompt(MESSAGES['incorrect_number'])
+      prompt('incorrect_number')
     end
   end
 
-  operator_prompt = <<-MSG
-    What operation would you like to perform?
-    1) add
-    2) subtract
-    3) multiply
-    4) divide
-  MSG
+  # operator_prompt = <<-MSG
+  #   What operation would you like to perform?
+  #   1) add
+  #   2) subtract
+  #   3) multiply
+  #   4) divide
+  # MSG
 
-  prompt(operator_prompt)
+  prompt('operator_prompt')
 
   operator = ''
   loop do
@@ -91,11 +108,13 @@ loop do # main loop
     if %w(1 2 3 4).include?(operator)
       break
     else
-      prompt(MESSAGES['one_to_four_choice'])
+      prompt('one_to_four_choice')
     end
   end
 
-  prompt("#{operation_to_message(operator)} " + MESSAGES['two_numbers'])
+  #prompt("#{operation_to_message(operator)} " + messages('two_numbers'))
+
+  operation_to_message(operator) #this is where you left off
 
   result = case operator
            when '1'
@@ -108,11 +127,12 @@ loop do # main loop
              number1.to_f() / number2.to_f()
            end
 
-  prompt((MESSAGES['result'] + "#{result}"))
+  #prompt(('result') + "#{result}")
+  puts "The result is #{result}"
 
-  prompt(MESSAGES['another_one'])
+  prompt('another_one')
   answer = Kernel.gets().chomp
   break unless answer.downcase().start_with?('y')
 end
 
-prompt(MESSAGES['good_bye'])
+prompt('good_bye')
