f9fa961fc4e7b817edcea963bcb81e80897aae83
Jordan Adamson <jordanadamson540@gmail.com>
2024-04-20 18:07:49 -0500

    most of code review changes and spellcheck

--- rb_101/lesson_2/jordan_adamson_rock_paper_scissors.rb
+++ rb_101/lesson_2/jordan_adamson_rock_paper_scissors.rb
@@ -5,7 +5,7 @@ VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard']
 RESET_SCORE = 0
 WINNER_TOTAL_SCORE = 3
 ONE_MATCHUP_POINT_ADDER = 1
-PAUSE_TIME = 3
+PAUSE_TIME = 0
 
 # side effects
 
@@ -25,12 +25,12 @@ def prompts_various_wrong_user_names(name)
   end
 end
 
-def prompts_various_wrong_user_inputs(response)
+def prompts_various_wrong_user_inputs(response, shorthand_responses)
   if one_letter_s_given?(response)
     prompt('one_letter_s')
   elsif response.empty?
     prompt('left_empty')
-  elsif !acceptable_responses?(response) && response != 'help'
+  elsif !acceptable_responses?(response, shorthand_responses) && response != 'help'
     prompt('select_valid_option')
   end
 end
@@ -45,10 +45,10 @@ def prompt_what_beats_what
   prompt('rules_of_game')
 end
 
-def display_results(player, computer)
-  if win?(player, computer)
+def display_results(player, computer, what_beats_what)
+  if win?(player, computer, what_beats_what)
     prompt('win')
-  elsif win?(computer, player)
+  elsif win?(computer, player, what_beats_what)
     prompt('cpu_win')
   else
     prompt("tie")
@@ -65,74 +65,14 @@ end
 
 # logic for player response inputs
 
-def acceptable_responses?(user_response)
-  return true if shorthand_for_rock_paper_lizard?(user_response)
-  return true if shorthand_for_spock_or_scissors?(user_response)
-  return true if VALID_CHOICES.include?(user_response)
-  false
+def acceptable_responses?(user_response, shorthand_responses)
+  shorthand_responses.any? do |key, value| 
+    value.match?(user_response)
+  end
 end
 
 def one_letter_s_given?(response)
   /^[s]$/i.match?(response)
-end
-
-def shorthand_for_rock_paper_lizard?(response)
-  return true if shorthand_rock_checker?(response)
-  return true if shorthand_paper_checker?(response)
-  return true if shorthand_lizard_checker?(response)
-  return true if response.include?('rock')
-  return true if response.include?('paper')
-  return true if response.include?('lizard')
-  false
-end
-
-def shorthand_for_spock_or_scissors?(response)
-  return true if shorthand_spock_checker?(response)
-  return true if shorthand_scissors_checker?(response)
-  return true if response.include?('spock')
-  return true if response.include?('scissors')
-  false
-end
-
-def shorthand_spock_checker?(response)
-  return true if /^sp$/i.match?(response)
-  return true if /^spo$/i.match?(response)
-  return true if /^spoc$/i.match?(response)
-  false
-end
-
-def shorthand_scissors_checker?(response)
-  return true if /^sc$/i.match?(response)
-  return true if /^sci$/i.match?(response)
-  return true if /^scis$/i.match?(response)
-  return true if /^sciss$/i.match?(response)
-  return true if /^scisso$/i.match?(response)
-  return true if /^scissor$/i.match?(response)
-  false
-end
-
-def shorthand_rock_checker?(response)
-  return true if /^r$/i.match?(response)
-  return true if /^ro$/i.match?(response)
-  return true if /^roc$/i.match?(response)
-  false
-end
-
-def shorthand_paper_checker?(response)
-  return true if /^p$/i.match?(response)
-  return true if /^pa$/i.match?(response)
-  return true if /^pap$/i.match?(response)
-  return true if /^pape$/i.match?(response)
-  false
-end
-
-def shorthand_lizard_checker?(response)
-  return true if /^l$/i.match?(response)
-  return true if /^li$/i.match?(response)
-  return true if /^liz$/i.match?(response)
-  return true if /^liza$/i.match?(response)
-  return true if /^lizar$/i.match?(response)
-  false
 end
 
 # small logic
@@ -153,48 +93,28 @@ def delay_time_seconds(seconds)
   sleep(seconds)
 end
 
-def full_name_generator(user_response)
-  if shorthand_rock_checker?(user_response)
+def full_name_generator(user_response, shorthand_responses)
+  if shorthand_responses[:rock].match?(user_response)
     'rock'
-  elsif shorthand_paper_checker?(user_response)
+  elsif shorthand_responses[:paper].match?(user_response)
     'paper'
-  elsif shorthand_scissors_checker?(user_response)
+  elsif shorthand_responses[:scissors].match?(user_response)
     'scissors'
-  elsif shorthand_spock_checker?(user_response)
+  elsif shorthand_responses[:spock].match?(user_response)
     'spock'
-  elsif shorthand_lizard_checker?(user_response)
+  elsif shorthand_responses[:lizard].match?(user_response)
     'lizard'
   end
 end
 
-def win?(first, second)
+def win?(first, second, what_beats_what)
   case first
-  when 'rock'     then rock_beats_?(second)
-  when 'paper'    then paper_beats_?(second)
-  when 'scissors' then scissors_beats_?(second)
-  when 'spock'    then spock_beats_?(second)
-  when 'lizard'   then lizard_beats_?(second)
+  when 'rock'     then what_beats_what[:rock].include?(second)
+  when 'paper'    then what_beats_what[:paper].include?(second)
+  when 'scissors' then what_beats_what[:scissors].include?(second)
+  when 'spock'    then what_beats_what[:spock].include?(second)
+  when 'lizard'   then what_beats_what[:lizard].include?(second)
   end
-end
-
-def rock_beats_?(second)
-  ['scissors', 'lizard'].include?(second)
-end
-
-def paper_beats_?(second)
-  ['rock', 'spock'].include?(second)
-end
-
-def scissors_beats_?(second)
-  ['paper', 'lizard'].include?(second)
-end
-
-def spock_beats_?(second)
-  ['rock', 'scissors'].include?(second)
-end
-
-def lizard_beats_?(second)
-  ['paper', 'spock'].include?(second)
 end
 
 # large logic
@@ -203,10 +123,11 @@ def continuously_updating_score_keeper(user_response,
                                        computer_choice,
                                        user_score,
                                        computer_score,
-                                       ties)
-  if win?(user_response, computer_choice)
+                                       ties,
+                                       what_beats_what)
+  if win?(user_response, computer_choice, what_beats_what)
     user_score += ONE_MATCHUP_POINT_ADDER
-  elsif win?(computer_choice, user_response)
+  elsif win?(computer_choice, user_response, what_beats_what)
     computer_score += ONE_MATCHUP_POINT_ADDER
   else
     ties += ONE_MATCHUP_POINT_ADDER
@@ -219,15 +140,12 @@ def user_or_computer_declared_winner?(player_score, computer_score)
 end
 
 def reset_scores
-  reset_player_score = RESET_SCORE
-  reset_computer_score = RESET_SCORE
-  reset_ties = RESET_SCORE
-  [reset_player_score, reset_computer_score, reset_ties]
+  [RESET_SCORE, RESET_SCORE, RESET_SCORE]
 end
 
 # high level abstracted logic
 
-def aquire_name
+def acquire_name
   loop do
     prompt('welcome_and_name')
     user_name = response
@@ -237,17 +155,17 @@ def aquire_name
   end
 end
 
-def aquire_user_response
+def acquire_user_response(shorthand_responses)
   loop do
     prompt('game_options_or_help')
     user_response = response.downcase
-    prompts_various_wrong_user_inputs(user_response)
+    prompts_various_wrong_user_inputs(user_response, shorthand_responses)
     prompt_how_to_play(user_response)
-    break user_response if acceptable_responses?(user_response)
+    break user_response if acceptable_responses?(user_response, shorthand_responses)
   end
 end
 
-def aquire_play_again
+def acquire_play_again
   loop do
     play_again_response = response.downcase.chr
     break play_again_response if %w(y n).include?(play_again_response)
@@ -255,44 +173,62 @@ def aquire_play_again
   end
 end
 
-def aquire_user_and_computer_responses
-  user_response = aquire_user_response
-  user_response = full_name_generator(user_response) \
+def acquire_user_and_computer_responses(shorthand_responses, what_beats_what)
+  user_response = acquire_user_response(shorthand_responses)
+  user_response = full_name_generator(user_response, shorthand_responses) \
     unless VALID_CHOICES.include?(user_response)
   computer_choice = VALID_CHOICES.sample
   prompt('user_chose_computer_chose', user_response, computer_choice)
   delay_time_seconds(PAUSE_TIME)
-  display_results(user_response, computer_choice)
+  display_results(user_response, computer_choice, what_beats_what)
   delay_time_seconds(PAUSE_TIME)
   [user_response, computer_choice]
 end
 
-def aquire_name_and_how_to_play
+def acquire_name_and_how_to_play
   clear_screen
-  user_name = aquire_name
+  user_name = acquire_name
   prompt('greeting_with_name', user_name)
   delay_time_seconds(PAUSE_TIME)
   prompt_how_to_play('help')
+  user_name
 end
 
 # start of program
+
+shorthand_responses = {
+                        rock: /^ro?c?k?$/i,
+                        paper: /^pa?p?e?r?$/i,
+                        scissors: /^sc+i?s?s?o?r?s?$/i,
+                        lizard: /^li?z?a?r?d?$/i,
+                        spock: /^sp+o?c?k?$/i
+                      }
+        
+what_beats_what = {
+                    rock: ['scissors', 'lizard'],
+                    paper: ['rock', 'spock'],
+                    scissors: ['paper', 'lizard'],
+                    lizard: ['paper', 'spock'],
+                    spock: ['rock', 'scissors']
+                  }
 
 user_name = ''
 player_score = RESET_SCORE
 computer_score = RESET_SCORE
 ties = RESET_SCORE
 
-user_name = aquire_name_and_how_to_play
+user_name = acquire_name_and_how_to_play
 
 loop do
   loop do
-    user_response, computer_choice = aquire_user_and_computer_responses
+    user_response, computer_choice = acquire_user_and_computer_responses(shorthand_responses, what_beats_what)
     player_score, computer_score, ties = \
       continuously_updating_score_keeper(user_response,
                                          computer_choice,
                                          player_score,
                                          computer_score,
-                                         ties)
+                                         ties,
+                                         what_beats_what)
     prompt('current_score', player_score, computer_score, ties)
     delay_time_seconds(PAUSE_TIME)
     break if user_or_computer_declared_winner?(player_score, computer_score)
@@ -301,10 +237,11 @@ loop do
   player_score, computer_score, ties = reset_scores
 
   prompt('ask_play_again')
-  play_again_response = aquire_play_again
+  play_again_response = acquire_play_again
   clear_screen
   break unless play_again_response.start_with?('y')
 end
 
 clear_screen
 prompt('goodbye_with_name', user_name)
+# 300 lines
